665542b4aec897d58ed67a2dedc724aa91abb9cb
amir-m-milani <amirmilani08@gmail.com>
2023-06-16 02:06:03 +0330

    create main part of app

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter
+from add_menu import *
+
+# Main window
+main_menu = tkinter.Tk()
+main_menu.geometry("150x150")
+main_menu.title("Main Menu")
+# Main Buttons
+add_btn = tkinter.Button(main_menu, text="Add",
+                         width=20, command=open_add_menu)
+add_btn.grid(row=0, column=0)
+edit_btn = tkinter.Button(main_menu, text="Edit",
+                          width=20, command=edit_add_menu)
+edit_btn.grid(row=1, column=0)
+
+
+if __name__ == "__main__":
+    main_menu.mainloop()
