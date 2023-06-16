0ac002b3a2dcb67a08bb005812e77d49af8668f6
amir-m-milani <amirmilani08@gmail.com>
2023-06-16 12:58:02 +0330

    make SQLlite class and add database to .gitingnore

---
+++ SQLConnect.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+class SQLConnect:
+    connection: sqlite3.connect
+    cur: sqlite3.Cursor
+
+    def __init__(self, name: str):
+        self.connection = sqlite3.connect(f"{name}.db")
+        self.cur = self.connection.cursor()
+
+    def sql_query(self, query: str):
+        self.cur.execute(query)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
