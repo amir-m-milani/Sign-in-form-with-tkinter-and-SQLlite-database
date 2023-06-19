bcd202d5a212c44bf26ee4932befeb12026a4e12
amir-m-milani <amirmilani08@gmail.com>
2023-06-19 22:32:57 +0330

    make all the checks and edit is complete

--- menus.py
+++ menus.py
@@ -46,25 +46,37 @@ def edit_user(id: str, fName: str, lName: str, sexuality: bool, age: str, salary
             if fName.isalpha():
                 connection.sql_query(
                     f"UPDATE users SET first_name = '{fName}' WHERE id = '{int(id)}'")
+            else:
+                errors.append("first name should be alphabtes")
         if not lName == "":
             if lName.isalpha():
                 connection.sql_query(
                     f"UPDATE users SET last_name = '{lName}' WHERE id = {int(id)}")
+            else:
+                errors.append("last name should be alphabets")
         if not sexuality == " ":
             connection.sql_query(
                 f"UPDATE users SET sexuality = '{sexuality}' WHERE id = {int(id)}")
+        else:
+            errors.append("you should choose your sex")
         if not age == "":
             if age.isnumeric():
                 connection.sql_query(
                     f"UPDATE users SET age = {int(age)} WHERE id = {int(id)}")
+            else:
+                errors.append("age must be numbers")
         if not salary == "":
             if salary.isnumeric():
                 connection.sql_query(
                     f"UPDATE users SET salary = {salary} WHERE id = {int(id)}")
+            else:
+                errors.append("salary must be numbers")
         if not weight == "":
             if weight.isnumeric():
                 connection.sql_query(
                     f"UPDATE users SET weight = {weight} WHERE id = {int(id)}")
+            else:
+                errors.append("weight should be numbers")
 
     else:
         errors.append("id has not been found!")
